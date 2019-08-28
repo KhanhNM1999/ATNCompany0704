fc809898bc6c700021672191891bbf10620ed732
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-29 03:07:36 +0700

    Update signinprocess.php

--- signinprocess.php
+++ signinprocess.php
@@ -3,15 +3,26 @@
 	$username = $_POST['userid'];
 	$password = $_POST['pass'];
 
-	$sql = "SELECT COUNT(*) FROM user WHERE userid='".$username."' AND password='".$password."'";
+	// $sql = "SELECT COUNT(*) FROM user WHERE userid='".$username."' AND password='".$password."'";
 
-
-	// $sql = "SELECT * FROM users WHERE userid = 'hnl' AND password = '12d'";
+	$sql = "select * from users where username = '$username' and password = '$password' ";
 	$stmt = $pdo->prepare($sql);
-
-//$stmt->setFetchMode(PDO::FETCH_ASSOC);
-	$stmt->execute();
+	$query = mysqli_query($conn,$sql);
+	$num_rows = mysqli_num_rows($query);
+			if ($num_rows==0) {
+				echo "tên đăng nhập hoặc mật khẩu không đúng !";
+			}else{
+				//tiến hành lưu tên đăng nhập vào session để tiện xử lý sau này
+				$_SESSION['username'] = $username;
+                // Thực thi hành động sau khi lưu thông tin vào session
+                // ở đây mình tiến hành chuyển hướng trang web tới một trang gọi là index.php
+                header('Location: index.php');
+	// $sql = "SELECT * FROM users WHERE userid = 'hnl' AND password = '12d'";
 	
+
+/* /$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+
 	$count = $stmt->fetchColumn();
 
 if($count > 0)
@@ -22,4 +33,6 @@ else
 {
 	echo "Fail con mẹ mày rồi";
 }
+*/
+
 ?>
