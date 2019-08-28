02163cfc112aed76b6a068fe913e9b317b324167
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-29 02:52:27 +0700

    Update signinprocess.php

--- signinprocess.php
+++ signinprocess.php
@@ -1,15 +1,17 @@
 <?php
 	require_once'./database.php';
-	$username = $_POST['user'];
+	$username = $_POST['userid'];
 	$password = $_POST['pass'];
 
-	$sql = "SELECT COUNT(*) FROM user WHERE userid='".$username."' AND password= '".$password."'";
+	$sql = "SELECT COUNT(*) FROM user WHERE userid='".$username."' AND password='".$password."'";
 
-//	$sql = "SELECTION * FROM user WHERE userid = 'teamwork1201' AND password='deochoaihet1';
 
+	// $sql = "SELECT * FROM users WHERE userid = 'hnl' AND password = '12d'";
 	$stmt = $pdo->prepare($sql);
 
+//$stmt->setFetchMode(PDO::FETCH_ASSOC);
 	$stmt->execute();
+	
 	$count = $stmt->fetchColumn();
 
 if($count > 0)
