54baf885ce70646b3ca0c00a3f2aae40753abdaf
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-29 16:04:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require_once 'database.php';
 if(isset($_POST["username"]) && isset($_POST["pass"]))
 {
 	$user = $_POST["username"];
-	$pass = $_POST["pass"];
+	$pass = $_POST["password"];
 	$sql ="SELECT * FROM user WHERE userid = '$user' AND pass= '$pass'";
 	$rows = pg_query($sql);
 	if(pg_num_rows($rows)==1) { ?>
