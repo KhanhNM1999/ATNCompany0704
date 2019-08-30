84621d4957b50142c968750f7a791c425bd7efd2
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-30 16:26:09 +0700

    Update login.php

--- login.php
+++ login.php
@@ -18,5 +18,12 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
                 window.location.href = "/index.php";
             </script>
         <?php }
+    $row = pg_fetch_array($query);
+     
+    //So sánh 2 mật khẩu có trùng khớp hay không
+    if ($password != $row['password']) {
+        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
 }
 ?>
