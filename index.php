5bb83a21022f2b787d9a95019b2d3b4d78d8ad23
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-31 01:51:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -189,25 +189,7 @@ table, th, td {
       $stmt3->execute(); 
       $arr3 =  $stmt3->fetch(PDO::FETCH_ASSOC);
 
-      $stmt4 = $pdo->prepare($sql4);
-      $stmt4->execute(); 
-      $arr4 =  $stmt4->fetch(PDO::FETCH_ASSOC);
-
-      $stmt5 = $pdo->prepare($sql5);
-      $stmt5->execute(); 
-      $arr5 =  $stmt5->fetch(PDO::FETCH_ASSOC);
-
-      $stmt6 = $pdo->prepare($sql6);
-      $stmt6->execute(); 
-      $arr6 =  $stmt6->fetch(PDO::FETCH_ASSOC);
-
-      $stmt7 = $pdo->prepare($sql7);
-      $stmt7->execute(); 
-      $arr7 =  $stmt7->fetch(PDO::FETCH_ASSOC);
-
-      $stmt8 = $pdo->prepare($sql8);
-      $stmt8->execute(); 
-      $arr8 =  $stmt8->fetch(PDO::FETCH_ASSOC);
+     
     ?>
 <table>
   
@@ -226,34 +208,7 @@ table, th, td {
         <img src= " <?php echo $arr3['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
         <span> <?php echo $arr3['productname']?> </span></br>
         <span><?php echo $arr3['price']; ?></span>
-      </th>
-      <th>
-        <img src= " <?php echo $arr4['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
-        <span> <?php echo $arr4['productname']?> </span></br>
-        <span><?php echo $arr4['price']; ?></span>
-      </th>      
-    </tr>
-    <tr>
-      <th>
-        <img src= " <?php echo $arr5['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
-        <span> <?php echo $arr5['productname']?> </span></br>
-        <span><?php echo $arr5['price']; ?></span>
-      </th>
-      <th>
-        <img src= " <?php echo $arr6['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
-        <span> <?php echo $arr6['productname']?> </span></br>
-        <span><?php echo $arr6['price']; ?></span>
-      </th>
-      <th><img src= " <?php echo $arr7['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
-        <span> <?php echo $arr7['productname']?> </span></br>
-        <span><?php echo $arr7['price']; ?></span>
-      </th>
-      <th>
-        <img src= " <?php echo $arr8['img']; ?>" style="width: 200 px; height: 250px" alt="placeholder+image"></br>
-        <span> <?php echo $arr8['productname']?> </span></br>
-        <span><?php echo $arr8['price']; ?></span>
-      </th>
-    </tr>
+      </th>       
 </table>
   </article>
 </section>
