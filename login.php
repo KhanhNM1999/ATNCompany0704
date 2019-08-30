cb7a1397255af6db4480ef68f20da7fe1b439ffd
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-30 16:10:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
-require_once ('database.php');  
-if(isset($_POST["username"]) && isset($_POST["pass"]))
+require_once'./database.php'; 
+if(isset($_POST["username"]) && isset($_POST["password"]))
 {
 	$user = $_POST["username"];
 	$pass = $_POST["password"];
