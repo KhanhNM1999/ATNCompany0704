be2518bb90982b4c6c621aeccef929626ce21b4e
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-08-30 16:31:41 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
 {
 	$user = $_POST["username"];
 	$pass = $_POST["password"];
-	$sql ="SELECT * FROM user WHERE userid = '$user' AND pass= '$pass'";
+	$sql ="SELECT * FROM user WHERE userid = '{$user}' AND pass= '{$pass}'";
 	$rows = pg_query($sql);
 	if(pg_num_rows($rows)==1) { ?>
 		<script>
@@ -18,12 +18,5 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
                 window.location.href = "/index.php";
             </script>
         <?php }
-    $row = pg_fetch_array($query);
-     
-    //So sánh 2 mật khẩu có trùng khớp hay không
-    if ($password != $row['password']) {
-        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-        exit;
-    }
 }
 ?>
