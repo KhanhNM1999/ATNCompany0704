d87bc34531e7f2699896231e67bbfb37aa52cad1
Nguyen Minh Khanh <edgpawn1999@gmail.com>
2019-09-01 13:27:22 +0700

    Update database.php

--- database.php
+++ database.php
@@ -9,6 +9,10 @@ try{
     $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
   // DELETE 4 LINES BELOW :
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
 echo 'Connection failed: ' . $e->getMessage();
